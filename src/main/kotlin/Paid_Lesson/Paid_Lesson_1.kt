a5fe9bc3f579af7e02e8e34a77f6c78294e50356
Nikita <Nikita@>
2025-07-03 22:49:24 +0300

    PD-1-1 do smth
    Signed-off-by: Nikita <Nikita@>

---
+++ src/main/kotlin/Paid_Lesson/Paid_Lesson_1.kt
@@ -0,0 +1,17 @@
+package org.example.Paid_Lesson
+
+fun main ( ) {
+
+
+    var numberOfOrders : Int  = 72
+    val welcomeMessage : String = ("Thank you for purshase")
+
+    println(numberOfOrders)
+
+    numberOfOrders = 52
+    println(numberOfOrders)
+    println(welcomeMessage)
+
+
+
+}
