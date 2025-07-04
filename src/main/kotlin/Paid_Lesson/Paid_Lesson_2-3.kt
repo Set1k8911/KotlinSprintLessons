d4510692aa29c0536e1952a1a80f55d63ac9b13c
Nikita <Nikita@>
2025-07-04 20:05:32 +0300

    PD-2-3 Train
    Signed-off-by: Nikita <Nikita@>

---
+++ src/main/kotlin/Paid_Lesson/Paid_Lesson_2-3.kt
@@ -0,0 +1,25 @@
+package org.example.Paid_Lesson
+
+import java.time.LocalTime
+import java.time.Period
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+fun main () {
+
+val start = (9*60) + 39
+    val  a: Duration = start.minutes
+    val  drive: Duration = TOTAL_TIME.minutes
+
+       println(a + drive)
+
+
+
+
+
+
+
+}
+const val TOTAL_TIME = 457
