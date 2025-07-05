4dbf871cc31e44ded53e544519f3554b8dc66225
Nikita <Nikita@>
2025-07-05 20:07:28 +0300

    KS-1-5 Smth
    Signed-off-by: Nikita <Nikita@>

--- src/main/kotlin/Paid_Lesson/Paid_Lesson_1_5.kt
+++ src/main/kotlin/Paid_Lesson/Paid_Lesson_1_5.kt
@@ -7,5 +7,5 @@ fun main() {
     val second = SECOND%SECOND
     println("%02d:%02d:%02d".format(hour, minute, second))
 }
- const val TIME = 60
- const val SECOND = 6480
+ const val TIME=60
+ const val SECOND=6480
