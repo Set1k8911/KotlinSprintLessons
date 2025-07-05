a425dc959c24052c6064e4a3b7af315017e38323
Nikita <Nikita@>
2025-07-05 20:12:26 +0300

    KS-2-3 Smth
    Signed-off-by: Nikita <Nikita@>

--- src/main/kotlin/Paid_Lesson/KS2-3.kt
+++ src/main/kotlin/Paid_Lesson/KS2-3.kt
@@ -4,13 +4,13 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 fun main () {
-    val startHour = 9
-    val startMin = 39
-val start = (startHour*MIN) + startMin
-    val  a: Duration = start.minutes
-    val  drive: Duration = TOTAL_TIME.minutes
-print("Поезд прибудет в ")
-       print( a + drive)
+    val startHour=9
+    val startMin=39
+    val start = (startHour*MIN)+startMin
+    val  startOfMotion: Duration=start.minutes
+    val  drive: Duration=TOTAL_TIME.minutes
+    print("Поезд прибудет в ")
+    print( startOfMotion + drive)
 }
-const val TOTAL_TIME = 457
-const val MIN = 60
+    const val TOTAL_TIME=457
+    const val MIN=60
