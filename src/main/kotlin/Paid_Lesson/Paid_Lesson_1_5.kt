27322a73750250b46a1dad3202b66ec2dc20212d
Nikita <Nikita@>
2025-07-05 19:59:12 +0300

    KS-1-5 Train
    Signed-off-by: Nikita <Nikita@>

--- src/main/kotlin/Paid_Lesson/Paid_Lesson_1_5.kt
+++ src/main/kotlin/Paid_Lesson/Paid_Lesson_1_5.kt
@@ -2,18 +2,10 @@ package org.example.Paid_Lesson
 
 fun main() {
 
-    val hour = SECOND/3600
-    val minute = (6480/TIME)%TIME
+    val hour = SECOND/(TIME*TIME)
+    val minute = (SECOND/TIME)%TIME
     val second = SECOND%SECOND
-
- println("%02d:%02d")
-
     println("%02d:%02d:%02d".format(hour, minute, second))
-
-
-
-
-
 }
  const val TIME = 60
-const val SECOND = 6480
+ const val SECOND = 6480
